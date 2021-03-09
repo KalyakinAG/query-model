dcf7edf379a3c374b61b335b551cf63c5d638276
Andrey Kalyakin <andrey.kalyakin@gmail.com>
2021-03-09 17:12:07 +0300

    Изменен путь сборки файла конфигурации

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
 rem https://github.com/best-tech/cfe2cf
-.\tools\cfe2cf.exe f .\bin\МодельЗапроса.cfe МодельЗапроса .\bin\МодельЗапроса.cf
+rem .\tools\cfe2cf.exe f .\bin\МодельЗапроса.cfe МодельЗапроса .\bin\МодельЗапроса.cf
+.\tools\cfe2cf.exe f .\bin\QueryModel.cfe МодельЗапроса .\bin\QueryModel.cf
