1a61d7786628a2c9d82e8a92ee7d2fc79158ea36
Andrey Kalyakin <andrey.kalyakin@gmail.com>
2021-06-17 19:51:00 +0300

    Рефакторинг

--- build.cmd
+++ build.cmd
@@ -1,3 +1,1 @@
-rem https://github.com/best-tech/cfe2cf
-rem .\tools\cfe2cf.exe f .\bin\МодельЗапроса.cfe МодельЗапроса .\bin\МодельЗапроса.cf
 .\tools\cfe2cf.exe f .\bin\QueryModel.cfe МодельЗапроса .\bin\QueryModel.cf
